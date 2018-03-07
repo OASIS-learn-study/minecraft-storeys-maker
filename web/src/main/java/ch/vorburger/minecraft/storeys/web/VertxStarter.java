91bd301471635a2f43ec70838fa34e96a22933b0
Michael Vorburger <mike@vorburger.ch>
2018-03-07 23:43:44 +0100

    {WIP} web test

--- web/src/main/java/ch/vorburger/minecraft/storeys/web/VertxStarter.java
+++ web/src/main/java/ch/vorburger/minecraft/storeys/web/VertxStarter.java
@@ -18,16 +18,12 @@
  */
 package ch.vorburger.minecraft.storeys.web;
 
-import static java.nio.charset.Charset.defaultCharset;
-
 import io.vertx.core.AsyncResult;
 import io.vertx.core.DeploymentOptions;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.VertxOptions;
 import io.vertx.core.logging.SLF4JLogDelegateFactory;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.concurrent.CompletableFuture;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -82,18 +78,6 @@ public class VertxStarter implements EventBusSender {
     public void send(Object message) {
         LOG.info("Sending message: {}", message);
         verticle.send(message);
-    }
-
-    // This main() is only for quick local testing; the Minecraft Sponge plugin directly uses above and not this
-    public static void main(String[] args) throws Exception {
-        VertxStarter starter = new VertxStarter();
-        starter.start(8080, new ActionsConsumer(null, null, null, null, null)).get();
-
-        System.out.println("Running now... press Enter to Stop.");
-        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in, defaultCharset()));
-        buffer.readLine();
-
-        starter.stop();
     }
 
 }
