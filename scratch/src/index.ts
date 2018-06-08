20177a6eec031b897151586f9b80075bec213926
Michael Vorburger <mike@vorburger.ch>
2018-06-09 01:17:59 +0200

    make ext.when_command only call minecraft.newCommand() once

--- scratch/src/index.ts
+++ scratch/src/index.ts
@@ -1,7 +1,5 @@
 import * as EventBus from 'vertx3-eventbus-client';
 import { Minecraft } from '../../api/build/classes/java/main/Minecraft-ts/minecraft-proxy';
-// TODO remove, probably; because already import'd in Minecraft:
-// import { CommandRegistration } from '../../api/build/classes/java/main/Minecraft-ts/command_registration-proxy';
 import { JSEncrypt } from 'jsencrypt';
 
 let ScratchExtensions: any;
@@ -47,6 +45,7 @@ let ScratchExtensions: any;
     var eventsReceived = { };
     var player_last_joined;
     var registeredConditions = new Set();
+    var registeredCommands = new Set();
     var code;
     var token;
 
@@ -86,23 +85,27 @@ let ScratchExtensions: any;
     }
 
     ext.when_command = function(command) {
-        // TODO how do we adapt this async API to a synchronous one for Scratch v2 Hat blocks with no async/callback support? :-()
-        minecraft.newCommand(ext.scratchMinecraftExtension.code, command, (err:any, result:any) => {
-            if (err) {
-                console.log("newCommand reply with error: ", err);
-            } else {
-                console.log("newCommand registered OK, now adding CommandRegistration");
-                result.on((err:any, result:any) => {
-                    if (err) {
-                        console.log("newCommand CommandRegistration on() reply with error: ", err);
-                    } else {
-                        console.log("newCommand CommandRegistration on() HIT!!!");
-                    }
-                });
-            }
-        });
-        // return ext.whenCondition("newCmd" + command);
-        return false; // TODO !!!
+        if (!registeredCommands.has(command)) {
+            registeredCommands.add(command);
+            console.log("newCommand:", command)
+            // TODO how do we adapt this async API to a synchronous one for Scratch v2 Hat blocks with no async/callback support? :-()
+            minecraft.newCommand(code, command, (err:any, result:any) => {
+                if (err) {
+                    console.log("newCommand reply with error: ", err);
+                } else {
+                    console.log("newCommand registered OK, now adding CommandRegistration");
+                    result.on((err:any, result:any) => {
+                        if (err) {
+                            console.log("newCommand CommandRegistration on() reply with error: ", err);
+                        } else {
+                            console.log("newCommand CommandRegistration on() HIT!!!");
+                        }
+                    });
+                }
+            });
+            // return ext.whenCondition("newCmd" + command);
+            return false; // TODO !!!
+        }
     };
 
     ext.minecraftCommand = function(command) {
