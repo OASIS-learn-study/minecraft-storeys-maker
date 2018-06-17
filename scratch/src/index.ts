69102e662efc5a5a95b732662f78359ef297ecf1
Erik Jan de Wit <erikjan.dewit@gmail.com>
2018-06-17 09:36:04 +0200

    use had written wrapper instead of vertx code gen

--- scratch/src/index.ts
+++ scratch/src/index.ts
@@ -1,21 +1,21 @@
 import * as EventBus from 'vertx3-eventbus-client';
-import { Minecraft } from '../../api/build/classes/java/main/Minecraft-ts/minecraft-proxy';
+import { Minecraft, HandType } from '../../api/src/main/typescript/observable-wrapper';
 import { JSEncrypt } from 'jsencrypt';
 
 let ScratchExtensions: any;
 
-(function(ext: any) {
+(function (ext: any) {
     // Block and block menu descriptions <https://github.com/LLK/scratchx/wiki>
     var descriptor = {
         blocks: [
             // TODO Translate labels, like on https://github.com/jbaragry/mcpi-scratch/blob/master/mcpi-scratch.js
-            ["h", "when %m.event",      "when_event",             "event"],
-            ["h", "when %s %m.interaction", "when_entity",        "entity", "right clicked"],
+            ["h", "when %m.event", "when_event", "event"],
+            ["h", "when %s %m.interaction", "when_entity", "entity", "right clicked"],
             ["h", "when inside %n %n %n, %n %n %n", "when_inside"],
-            ["h", "Command /%s",        "when_command",           "demo"],
-            ["w", "%s speak %s",        "narrate",                "entity", "text"],
-            [" ", "/%s",                "minecraftCommand",       "command"],
-            ["w", "title %s",           "sendTitle",              "Welcome!"],
+            ["h", "Command /%s", "when_command", "demo"],
+            ["w", "%s speak %s", "narrate", "entity", "text"],
+            [" ", "/%s", "minecraftCommand", "command"],
+            ["w", "title %s", "sendTitle", "Welcome!"],
             ["r", "last joined Player", "get_player_last_joined"],
             ["R", "Item held", "get_player_item_held"],
             ["r", "%m.item", "get_item_name", "Apple"]
@@ -25,70 +25,56 @@ let ScratchExtensions: any;
             event: ["Player joins"],
             interaction: [/*"left clicked", */ "right clicked"],
             item: ["Apple", "Beef", "Beetroot", "Boat", "Book", "Bow", "Bowl", "Bread",
-                "Cactus", "Cake", "Carrot", "Cauldron", "Chicken", "Clock", "Cookie" ]
+                "Cactus", "Cake", "Carrot", "Cauldron", "Chicken", "Clock", "Cookie"]
         },
         url: "https://github.com/vorburger/minecraft-storeys-maker/"
     };
 
     var match,
-        pl     = /\+/g,  // Regex for replacing addition symbol with a space
+        pl = /\+/g,  // Regex for replacing addition symbol with a space
         search = /([^&=]+)=?([^&]*)/g,
         decode = function (s) { return decodeURIComponent(s.replace(pl, " ")); },
-        query  = window.location.search.substring(1);
+        query = window.location.search.substring(1);
 
     var urlParams: any = {};
     while (match = search.exec(query))
-    urlParams[decode(match[1])] = decode(match[2]);
+        urlParams[decode(match[1])] = decode(match[2]);
 
-    var minecraft : Minecraft;
+    var minecraft: Minecraft;
     var eb; // the Vert.X EventBus
-    var eventsReceived = { };
+    var eventsReceived = {};
     var player_last_joined;
     var registeredConditions = new Set();
     var code;
     var token;
 
-    descriptor.menus.event.forEach(function(eventLabel) {
+    descriptor.menus.event.forEach(function (eventLabel) {
         eventsReceived[eventLabel] = false;
     });
 
-    ext.sendTitle = function(sendTitle: string, callback: Function) {
-        minecraft.showTitle(token, sendTitle, (err: any, result: any) => {
-            // TODO edewit: factor this out into a ../../api/src/api-utils.ts (?) usable from all functions here as well as scratch3/
-            // see https://github.com/vorburger/minecraft-storeys-maker/issues/59 for some more ideas
-            if (err) {
-                console.log("sendTitle reply with error: ", err);
-            } else {
-                callback();
-            }
-        });
+    ext.sendTitle = (sendTitle: string, callback: Function) => {
+        minecraft.showTitle(token, sendTitle).subscribe(() => callback(),
+            (err: any) => console.log("sendTitle reply with error: ", err)
+        );
     };
-    ext.narrate = function(entity: string, text: string, callback: Function) {
-        minecraft.narrate(code, entity, text, (err: any) => {
-            if (err) {
-                console.log("narrate reply with error: ", err);
-            } else {
-                callback();
-            }
-        })
+    ext.narrate = (entity: string, text: string, callback: Function) => {
+        minecraft.narrate(code, entity, text).subscribe(() => callback(),
+            (err: any) => console.log("narrate reply with error: ", err)
+        );
     };
-    ext.get_player_item_held = function(callback: Function) {
-        minecraft.getItemHeld(code, "MainHand", (err: any, result: string) => {
-            if (err) {
-                console.log("getItemHeld reply with error: ", err);
-            } else {
-                callback(result);
-            }
-        })
+    ext.get_player_item_held = (callback: Function) => {
+        minecraft.getItemHeld(code, HandType.MainHand).subscribe(result => callback(result),
+            (err: any) => console.log("getItemHeld reply with error: ", err)
+        );
     }
-    ext.minecraftCommand = function(command) {
+    ext.minecraftCommand = function (command) {
         eb.send("mcs.actions", { "action": "command", "command": command, "code": code });
     };
 
     //
     // Hat Blocks Condition Events related stuff here...
     //
-    ext.eventReceived = function(message) {
+    ext.eventReceived = function (message) {
         console.log("Vert.x Event Bus received message: " + JSON.stringify(message));
         // because the descriptor.menus.event[] will be translated, we have to "map" these:
         if (message.body.event == "playerJoined") {
@@ -100,7 +86,7 @@ let ScratchExtensions: any;
             console.log("eventsReceived[" + message.body.event + "] = true");
         }
     };
-    ext.when_event = function(event) {
+    ext.when_event = function (event) {
         var was = eventsReceived[event];
         // TODO if was is null/nil (?) then false (and remove pre-init)
         if (was == true) {
@@ -109,9 +95,9 @@ let ScratchExtensions: any;
         eventsReceived[event] = false;
         return was;
     };
-    ext.whenCondition = function(condition) {
+    ext.whenCondition = function (condition) {
         if (!registeredConditions.has(condition)) {
-            eb.send("mcs.actions", { "action": "registerCondition", "condition": condition, "code": code }, function(reply) {
+            eb.send("mcs.actions", { "action": "registerCondition", "condition": condition, "code": code }, function (reply) {
             });
             // We do this immediately after instead of inside the callback above, because we want it only once,
             // and callback may take a moment, but this gets called a lot; the chance that the send() failed is minimal.
@@ -120,25 +106,25 @@ let ScratchExtensions: any;
             return ext.when_event(condition);
         }
     };
-    ext.when_command = function(command) {
+    ext.when_command = function (command) {
         return ext.whenCondition("newCmd" + command);
     };
-    ext.when_inside = function(x1, y1, z1, x2, y2, z2) {
+    ext.when_inside = function (x1, y1, z1, x2, y2, z2) {
         return ext.whenCondition("myPlayer_inside_" + x1 + "/" + y1 + "/" + z1 + "/" + x2 + "/" + y2 + "/" + z2 + "/");
     };
-    ext.when_entity = function(entity, interaction) {
+    ext.when_entity = function (entity, interaction) {
         return ext.whenCondition("entity_interaction:" + entity + "/" + interaction);
     };
-    ext.get_player_last_joined = function() {
+    ext.get_player_last_joined = function () {
         return player_last_joined;
     };
-    ext.get_item_name = function(itemName) {
+    ext.get_item_name = function (itemName) {
         // Support translations here too...
         return itemName;
     };
 
     // Cleanup function when the extension is unloaded
-    ext._shutdown = function() {
+    ext._shutdown = function () {
         // TODO eb. has no close(); ?!
     };
 
@@ -146,7 +132,7 @@ let ScratchExtensions: any;
     // Return any message to be displayed as a tooltip.
     // Use this to report missing hardware, plugin or unsupported browser
     // Status values: 0 = error (red), 1 = warning (yellow), 2 = ready (green)
-    ext._getStatus = function() {
+    ext._getStatus = function () {
         // TODO implement based on JS getScript loading and eventBus connection!
         return { status: 2, msg: "Ready" };
     };
@@ -154,8 +140,8 @@ let ScratchExtensions: any;
     var crypt = new JSEncrypt(512);
     eb = new EventBus(urlParams.eventBusURL);
     eb.enableReconnect(true);
-    minecraft = new Minecraft(eb, "ch.vorburger.minecraft.storeys");
-    eb.onopen = function() {
+    minecraft = new Minecraft(eb);
+    eb.onopen = function () {
         eb.registerHandler("mcs.events", function (error, message) {
             if (error != null) {
                 console.log("Vert.x Event Bus received error: " + error);
@@ -165,28 +151,26 @@ let ScratchExtensions: any;
         });
 
         if (typeof (urlParams.code !== 'undefined')) {
-            crypt.getKey(function() {
-                minecraft.login(urlParams.code, crypt.getPublicKeyB64(), (err, response) => {
-                    if (err) {
-                        console.log("login reply with error: ", err);
-                    } else {
-                        console.log("Logging in... secret", response.secret);
-                        var id = crypt.decrypt(response.secret);
-                        var key = response.key;
-                        crypt = new JSEncrypt();
-                        crypt.setPublicKey(key);
-                        code = crypt.encrypt(id);
-                        token = { loginCode: code };
-                        console.log("New style Token", token);
-                    }
-                });
+            crypt.getKey(() => {
+                minecraft.login(urlParams.code, crypt.getPublicKeyB64()).subscribe(response => {
+                    console.log("Logging in... secret", response.secret);
+                    var id = crypt.decrypt(response.secret);
+                    var key = response.key;
+                    crypt = new JSEncrypt();
+                    crypt.setPublicKey(key);
+                    this.code = crypt.encrypt(id);
+                    this.token = { loginCode: this.code };
+                    console.log("New style Token", this.token);
+                }, err =>
+                        console.log("login reply with error: ", err)
+                );
             });
         }
 
         eb.send("mcs.actions", { "action": "ping" });
         // TODO await "PONG" reply, and set status green
     };
-    eb.onclose = function() {
+    eb.onclose = function () {
         console.log("Vert.x Event Bus Connection Close");
     };
 
