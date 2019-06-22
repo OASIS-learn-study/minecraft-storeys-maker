95ce6b988df25bcf70f730613cd02321c20d49d4
Michael Vorburger ⛑️ <mike@vorburger.ch>
2019-06-22 15:28:01 +0000

    introduce engine/

---
+++ engine/src/main/java/study/learn/storeys/engine/DemoMain.java
@@ -0,0 +1,30 @@
+/*
+ * ch.vorburger.minecraft.storeys
+ *
+ * Copyright (C) 2016 - 2018 Michael Vorburger.ch <mike@vorburger.ch>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package study.learn.storeys.engine;
+
+public class DemoMain {
+
+    public String getGreeting() {
+        return "Hello world.";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new DemoMain().getGreeting());
+    }
+}
