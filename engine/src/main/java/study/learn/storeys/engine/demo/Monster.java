2ac2d66e87f4c36ec3f5d0f05b200316420b5862
Michael Vorburger ⛑️ <mike@vorburger.ch>
2019-06-28 16:37:49 +0000

    add Monster v0.1

---
+++ engine/src/main/java/study/learn/storeys/engine/demo/Monster.java
@@ -0,0 +1,57 @@
+/*
+ * ch.vorburger.minecraft.storeys
+ *
+ * Copyright (C) 2016 - 2018 Michael Vorburger.ch <mike@vorburger.ch>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package study.learn.storeys.engine.demo;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Random;
+
+import com.google.common.collect.ImmutableList;
+
+import study.learn.storeys.engine.Interactlet;
+import study.learn.storeys.engine.Prompter;
+
+public class Monster extends Interactlet {
+
+    Random rand = new Random();
+
+    int monsterHP = rand.nextInt(10) + 5;
+    int monsterATT = rand.nextInt(4) + 1;
+
+    List<String> bodyTypes = ImmutableList.of("Cow body", "Human body");
+    String body = random(bodyTypes);
+
+    List<String> headTypes = ImmutableList.of("Snake head", "Red gem looking head");
+    String head = random(headTypes);
+
+    @Override public void interact(Prompter<Void> prompter) throws IOException {
+        int playerHP = 10;
+        int playerATT = 3;
+
+        prompter.await(aChoice("You have " + playerHP + " HP, and " + playerATT
+            + " damage.  You are faced with a monster that has a " + body + " and a " + head
+            + ". Where will you attack?",
+            body, body,
+            head, head));
+    }
+
+    String random(List<String> pickFrom) {
+        return pickFrom.get(rand.nextInt(pickFrom.size()));
+    }
+}
