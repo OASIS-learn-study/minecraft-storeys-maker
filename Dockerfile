1e3526c7e5a3258867d64d5083eb9deb7f91d473
Michael Vorburger ⛑️ <mike@vorburger.ch>
2022-04-02 20:07:53 +0200

    Use custom server icon, MOTD and gamemode (#293)
    * Remove (broken) Travis CI badge from README
    
    * Use our great custom server-icon.png
    
    from https://github.com/OASIS-learn-study/swissarmyknife-minecraft-server/blob/master/src/server-icon.png
    from https://github.com/OASIS-learn-study/minecraft-storeys-maker/issues/74
    
    * Use custom MOTD
    
    * Use creative peaceful server.properties
    
    Based on https://github.com/OASIS-learn-study/s2i-minecraft-server/blob/master/src/server.properties,
    I've verified each of the properties we had there, but most of them were the defaults anyway, this is better.
    
    * Using 0/1 instead of peaceful/creative doesn't work either...
    
    ... because of https://github.com/itzg/docker-minecraft-server/issues/1452.
    
    * Use custom server.properties instead of MODE & DIFFICULTY environment variables
    
    because of https://github.com/itzg/docker-minecraft-server/issues/1452
    
    * Make GuardGameModeJoinListener log game mode changes
    
    * Fix quite confusing 2nd bug in GuardGameModeJoinListener

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,13 @@ RUN ./gradlew build -x test
 # Keep any changes made here strictly in sync with Dockerfile-local
 FROM itzg/minecraft-server:java8
 COPY --from=build /project/web/build/libs/*-all.jar /mods/
+COPY minecraft-server/* /data-init/
 
 ENV EULA=TRUE
 ENV TYPE=SPONGEVANILLA
+ENV SKIP_SERVER_PROPERTIES=true
 EXPOSE 25565 25575 7070 8080
-ENTRYPOINT [ "/start" ]
+ENTRYPOINT [ "/data-init/start-custom" ]
+
+# TODO When https://github.com/itzg/docker-minecraft-server/issues/1449,
+#      then "ENV ICON=/data-init/server-icon.png", but until then that's in the start-custom
--- storeys/src/main/java/ch/vorburger/minecraft/storeys/guard/GuardGameModeJoinListener.java
+++ storeys/src/main/java/ch/vorburger/minecraft/storeys/guard/GuardGameModeJoinListener.java
@@ -18,6 +18,8 @@
  */
 package ch.vorburger.minecraft.storeys.guard;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.spongepowered.api.data.key.Keys;
 import org.spongepowered.api.entity.living.player.Player;
 import org.spongepowered.api.entity.living.player.gamemode.GameMode;
@@ -32,22 +34,23 @@ import org.spongepowered.api.event.network.ClientConnectionEvent.Join;
  */
 public class GuardGameModeJoinListener implements EventListener<Join> {
 
-    @Override
-    public void handle(Join joinEvent) throws Exception {
+    private static final Logger LOG = LoggerFactory.getLogger(GuardGameModeJoinListener.class);
+
+    @Override public void handle(Join joinEvent) throws Exception {
         GameMode newGameMode = null;
         Player player = joinEvent.getTargetEntity();
         // NB: Order and use of if and not else if - because higher permission overrides lower...
+        // But beware that OPS on servers without permissions plugin (such as LuckPerms) have all permissions;
+        // therefore the last permission must be the one which we wants an OPS to have!
         if (player.hasPermission("storeys.guard.adventure")) {
             newGameMode = GameModes.ADVENTURE;
         }
         if (player.hasPermission("storeys.guard.creative")) {
             newGameMode = GameModes.CREATIVE;
         }
-        if (player.hasPermission("storeys.guard.survival")) {
-            newGameMode = GameModes.SURVIVAL;
-        }
         if (newGameMode != null) {
             player.offer(Keys.GAME_MODE, newGameMode);
+            LOG.info("Setting Player {} game mode to {} due to their storeys.guard.* permissions", player.getName(), newGameMode);
         }
     }
 
