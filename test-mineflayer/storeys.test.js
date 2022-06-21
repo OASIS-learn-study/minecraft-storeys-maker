8f1f3f588fc74539aecb7d6095bda9c9264eea8a
Erik Jan de Wit <erikjan.dewit@gmail.com>
2022-06-21 13:43:24 +0200

    added test case to mineflayer

--- test-mineflayer/storeys.test.js
+++ test-mineflayer/storeys.test.js
@@ -1,23 +1,34 @@
 const mineflayer = require('mineflayer');
 
+const TIMEOUT = 10 * 1000;
+
+const createBot = (command) => {
+  const bot = mineflayer.createBot({
+    host: "localhost",
+    port: 25565
+  });
+  bot.on("login", () => {
+    bot.chat(command);
+  });
+  return bot;
+}
+
+const handleError = (bot, done) => {
+  bot.on("error", (e) => {
+    done(e);
+  });
+}
+
 describe("Storeys plugin test", () => {
   beforeAll(() => {
     // Prevent jest from complaining about an import after the test is done (You are trying to `import` a file after the Jest environment has been torn down.)
     jest.useFakeTimers('legacy');
-    console.log = () => {};
+    console.log = () => { };
   });
 
   test("should connect to minecraft server and execute /make", (done) => {
     // given
-    const bot = mineflayer.createBot({
-      host: 'localhost',
-      port: 25565
-    });
-
-    // when
-    bot.on("login", () => {
-      bot.chat('/make');
-    });
+    const bot = createBot("/make");
 
     // then
     bot.on('messagestr', (msg) => {
@@ -28,8 +39,20 @@ describe("Storeys plugin test", () => {
       }
     });
 
-    bot.on("error", (e) => {
-      done(e);
+    handleError(bot, done);
+  }, TIMEOUT);
+
+  test("should execute /new", (done) => {
+    // given
+    const bot = createBot("/new");
+
+    // then
+    bot.on('title', (msg) => {
+      expect(msg).toEqual("{\"text\":\"Hello\"}");
+      done();
+      bot.quit();
     });
-  }, 10 * 1000);
+
+    handleError(bot, done);
+  }, TIMEOUT);
 });
