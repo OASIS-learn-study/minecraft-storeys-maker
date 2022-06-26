1b0854b9929b62a04847de667004c3d8a7e17fd8
Erik Jan de Wit <erikjan.dewit@gmail.com>
2022-06-26 22:54:42 +0200

    Improved the tests to also open the browser and create a `/demo` command execute this.

--- test-mineflayer/storeys.test.js
+++ test-mineflayer/storeys.test.js
@@ -1,4 +1,5 @@
 const mineflayer = require('mineflayer');
+const { spawn } = require('node:child_process');
 
 const expect = require('chai').expect
 
@@ -15,26 +16,39 @@ describe("Storeys plugin test", () => {
 
   after(() => bot.end());
 
-  it("should connect to minecraft server and execute /make", (done) => {
+  let bot;
+
+
+  afterAll(() => bot.quit());
+
+  test("1 should connect to minecraft server and execute /make", (done) => {
     // given
     bot.chat("/make");
 
     // then
     bot.on('messagestr', (msg, _, json) => {
       if (msg !== "Player joined the game") {
-        expect(msg).to.equal("Click here to open Scratch and MAKE actions");
+        loginURL = json.extra[0].extra[0].extra[0].extra[0].clickEvent.value;
         done();
       }
     });
   });
 
   it("should execute /new", (done) => {
+  test("2 should create new command /demo", (done) => {
+    const child = spawn('npx', ['cypress', 'run', '--browser', 'chrome'], { env: { ...process.env, CYPRESS_URL: loginURL } });
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
+    child.on('close', done);
+  }, 2 * TIMEOUT);
+
+  test("3 should execute /demo", (done) => {
     // given
-    bot.chat("/new");
+    bot = createBot("/demo");
 
     // then
     bot.on('title', (msg) => {
-      expect(msg).to.equal("{\"text\":\"Hello\"}");
+      expect(msg).toEqual("{\"text\":\"automated test!\"}");
       done();
     });
   });
