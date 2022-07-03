3c1711b18fb483ae96ffeadf852eeeb591238708
Erik Jan de Wit <erikjan.dewit@gmail.com>
2022-07-03 15:35:33 +0200

    added type information on the monaco editor

---
+++ blockly/src/monaco/CodeEditor.tsx
@@ -0,0 +1,32 @@
+import { useEffect } from "react";
+import Editor, { useMonaco } from "@monaco-editor/react";
+import Blockly from "blockly/core";
+
+import Minecraft from '../blockly/storeys/storeys.d.ts?raw';
+import { generate } from "../blockly/storeys/code";
+
+type EditorProps = {
+  workspace: Blockly.Workspace;
+};
+
+export const CodeEditor = ({ workspace }: EditorProps) => {
+  const monaco = useMonaco();
+
+  useEffect(() => {
+    (async () => {
+      monaco?.languages.typescript.javascriptDefaults.addExtraLib(
+        Minecraft,
+        "../blockly/storeys/storeys"
+      );
+      monaco?.editor.createModel(Minecraft, 'typescript', monaco.Uri.parse("../blockly/storeys/storeys"));
+    })();
+  }, [monaco]);
+
+  return (
+    <Editor
+      height="90vh"
+      defaultLanguage="javascript"
+      defaultValue={generate(workspace)}
+    />
+  );
+};
