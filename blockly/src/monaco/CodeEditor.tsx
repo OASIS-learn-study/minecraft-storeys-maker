b24a15042c872a1bf9010c471ca62f633931c0ab
Erik Jan de Wit <erikjan.dewit@gmail.com>
2022-07-04 07:01:23 +0200

    ignore raw import

--- blockly/src/monaco/CodeEditor.tsx
+++ blockly/src/monaco/CodeEditor.tsx
@@ -2,6 +2,7 @@ import { useEffect } from "react";
 import Editor, { useMonaco } from "@monaco-editor/react";
 import Blockly from "blockly/core";
 
+// @ts-ignore
 import Minecraft from '../blockly/storeys/storeys.d.ts?raw';
 import { generate } from "../blockly/storeys/code";
 
