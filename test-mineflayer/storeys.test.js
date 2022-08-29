3b88e9a68204437f71d3b9184d1367df5f107b69
Erik Jan de Wit <erikjan.dewit@gmail.com>
2022-08-29 20:33:30 +0200

    changed to use mocha instead of jest

--- test-mineflayer/storeys.test.js
+++ test-mineflayer/storeys.test.js
@@ -1,74 +1,51 @@
 const mineflayer = require('mineflayer');
 const { spawn } = require('node:child_process');
 
-const TIMEOUT = 10 * 1000;
-
-const executeCommandOnLogin = (command) => {
-  bot.on("login", () => {
-    bot.chat(command);
-  });
-}
-
-const handleError = (bot, done) => {
-  bot.on("error", (e) => {
-    done(e);
-  });
-}
+const expect = require('chai').expect
 
 describe("Storeys plugin test", () => {
-  beforeAll(() => {
-    // Prevent jest from complaining about an import after the test is done (You are trying to `import` a file after the Jest environment has been torn down.)
-    jest.useFakeTimers('legacy');
-    console.log = () => { };
-  });
-let bot;
+  let bot;
+  let loginURL;
 
-
-
-  beforeEach(() => {
+  before((done) => {
     bot = mineflayer.createBot({
       host: "localhost",
       port: 25565
     });
+    bot.on("login", done);
   })
 
-  afterEach(() => bot.quit());
+  after(() => bot.end());
 
-  test("1 should connect to minecraft server and execute /make", (done) => {
+  it("1 should connect to minecraft server and execute /make", (done) => {
     // given
-    executeCommandOnLogin("/make");
+    bot.chat("/make");
 
     // then
     bot.on('messagestr', (msg, _, json) => {
       if (msg !== "Player joined the game") {
         loginURL = json.extra[0].extra[0].extra[0].extra[0].clickEvent.value;
-        expect(msg).toEqual("Click here to open Scratch and MAKE actions");
+        expect(msg).to.equal("Click here to open Scratch and MAKE actions");
         done();
-        bot.quit();
       }
     });
+  });
 
-    handleError(bot, done);
-  }, TIMEOUT);
-
-  test("2 should create new command /demo", (done) => {
-    const child = spawn('npx', ['cypress', 'run', '--browser', 'electron'], { env: { ...process.env, CYPRESS_URL: loginURL } });
+  it("2 should create new command /demo", (done) => {
+    const child = spawn('npx', ['cypress', 'run'], { env: { ...process.env, CYPRESS_URL: loginURL } });
     child.stdout.pipe(process.stdout);
     child.stderr.pipe(process.stderr);
-    child.on('close', done);
-  }, 2 * TIMEOUT);
+    child.on('exit', done);
+  });
 
-  test("3 should execute /demo", (done) => {
+  it("3 should execute /demo", (done) => {
     // given
-    executeCommandOnLogin("/demo");
+    bot.chat("/demo");
 
     // then
     bot.on('title', (msg) => {
-      expect(msg).toEqual("{\"text\":\"automated test!\"}");
+      expect(msg).to.equal("{\"text\":\"automated test!\"}");
       done();
-      bot.quit();
     });
-
-    handleError(bot, done);
-  }, TIMEOUT);
+  });
 });
