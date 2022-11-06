17aea0b69a0a12cddf17c411232418292593b3ff
Erik Jan de Wit <erikjan.dewit@gmail.com>
2022-11-06 08:13:02 -0500

    Use playwright to do browser testing (#429)
    With some fixes, incl. installing Playwright dependencies into build container instead of at each test execution (by @vorburger, rest by @edewit)
    
    Co-authored-by: Michael Vorburger <mike@vorburger.ch>

--- test-mineflayer/storeys.test.js
+++ test-mineflayer/storeys.test.js
@@ -1,9 +1,11 @@
 const mineflayer = require('mineflayer');
+const { spawn } = require('node:child_process');
 
 const expect = require('chai').expect
 
 describe("Storeys plugin test", () => {
   let bot;
+  let loginURL;
 
   before((done) => {
     bot = mineflayer.createBot({
@@ -15,26 +17,34 @@ describe("Storeys plugin test", () => {
 
   after(() => bot.end());
 
-  it("should connect to minecraft server and execute /make", (done) => {
+  it("1 should connect to minecraft server and execute /make", (done) => {
     // given
     bot.chat("/make");
 
     // then
     bot.on('messagestr', (msg, _, json) => {
       if (msg !== "Player joined the game") {
+        loginURL = json.extra[0].extra[0].extra[0].extra[0].clickEvent.value;
         expect(msg).to.equal("Click here to open a browser and start MAKE actions");
         done();
       }
     });
   });
 
-  it("should execute /new", (done) => {
+  it("2 should create new command /demo", (done) => {
+    const child = spawn('npm', ['run', 'playwright'], { env: { ...process.env, URL: loginURL } });
+    child.stdout.pipe(process.stdout);
+    child.stderr.pipe(process.stderr);
+    child.on('exit', done);
+  });
+
+  it("3 should execute /demo", (done) => {
     // given
-    bot.chat("/new");
+    bot.chat("/demo");
 
     // then
     bot.on('title', (msg) => {
-      expect(msg).to.equal("{\"text\":\"Hello\"}");
+      expect(msg).to.equal("{\"text\":\"automated test!\"}");
       done();
     });
   });
